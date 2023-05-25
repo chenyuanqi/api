84638aadff3a01fcf6d83573d1da3d65d8dbccf7
chenyuanqi <chenyuanqi@outlook.com>
2023-05-25 17:15:49 +0800

    update: 添加注册验证请求

--- routes/api.go
+++ routes/api.go
@@ -4,6 +4,8 @@ package routes
 import (
 	"net/http"
 
+	"api/app/http/controllers/api/v1/auth"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,5 +22,14 @@ func RegisterAPIRoutes(r *gin.Engine) {
 				"Hello": "World!",
 			})
 		})
+
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 判断 Email 是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+		}
 	}
 }
